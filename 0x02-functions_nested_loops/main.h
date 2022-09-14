273639601a19191c1e19c6648f8c4e77d0c6b52d
isaacwrite <isaacwrite@gmail.com>
2022-09-13 17:18:54 -0700

    _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,10 +3,10 @@
 
 #include<stdio.h>
 
-int _putchar(char c) ;
+int _putchar(char c)  ;
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _i slower(int c);
+int _ i slower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
