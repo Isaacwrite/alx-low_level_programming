66be7f786bad19334542c0ed6195460fe016e89a
isaacwrite <isaacwrite@gmail.com>
2022-09-22 17:47:57 -0700

    Noise is a buffer, more effective than cubicles or booth walls

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,57 +1,41 @@
-/*
- * File: 103-print_buffer.c
- * Auth: Isaac O
- */
-
 #include "main.h"
 #include <stdio.h>
-
 /**
- * print_buffer - Prints a buffer 10 bytes at a time, starting with
- *                the byte position, then showing the hex content,
- *                then displaying printable charcaters.
- * @b: The buffer to be printed.
- * @size: The number of bytes to be printed from the buffer.
+ * print_buffer - function that prints a buffer
+ * @b: input
+ * @size: input
+ * Return: output
  */
 void print_buffer(char *b, int size)
 {
-	int byte, index;
-
-	for (byte = 0; byte < size; byte += 10)
-	{
-		printf("%08x: ", byte);
-
-		for (index = 0; index < 10; index++)
-		{
-			if ((index + byte) >= size)
-				printf("  ");
-
-			else
-				printf("%02x", *(b + index + byte));
-
-			if ((index % 2) != 0 && index != 0)
-				printf(" ");
-		}
-
-		for (index = 0; index < 10; index++)
-		{
-			if ((index + byte) >= size)
-				break;
-
-			else if (*(b + index + byte) >= 31 &&
-				 *(b + index + byte) <= 126)
-				printf("%c", *(b + index + byte));
-
-			else
-				printf(".");
-		}
-
-		if (byte >= size)
-			continue;
-
-		printf("\n");
-	}
+	int i, j, k, f;
 
 	if (size <= 0)
+	{
 		printf("\n");
+		return;
+	}
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
+		for (j = i; j < i + 10; j += 2)
+		{
+			for (k = j; k <= j + 1; k++)
+				if ((b[k] != 0) && (k < size))
+					printf("%02x", b[k]);
+				else if (k < size)
+					printf("%02x", 0);
+				else
+					printf("  ");
+			printf(" ");
+		}
+		for (f = i; f < i + 10; f++)
+		{
+			if ((b[f] >= 32 && b[f] <= 126) && (f < size))
+				printf("%c", b[f]);
+			else if (f < size)
+				printf("%c", '.');
+		}
+		printf("\n");
+	}
 }
