d25c59b122e433d94eb50502da54b61d9e091be9
isaacwrite <isaacwrite@gmail.com>
2022-09-30 07:59:00 -0700

    0. It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,39 +1,18 @@
+#include <stdio.h>
 #include "main.h"
+
 /**
- *create_array - array for prints a string
- *@size: number elements array
- *@c: char
- *Return: pointer
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
  */
 
-char *create_array(unsigned int size, char c)
+int main(int argc, char *argv[])
 {
-char *buffer;
-unsigned int position;
+(void) argc;
+printf("%s\n", argv[0]);
 
-if (size == 0)
-{
-return (NULL);
-}
-
-/*Define values with malloc*/
-buffer = (char *) malloc(size * sizeof(c));
-
-if (buffer == 0)
-{
-return (NULL);
-}
-
-else
-{
-position = 0;
-while (position < size) /*While for array*/
-{
-*(buffer + position) = c;
-position++;
-}
-
-return (buffer);
-}
-
+return (0);
 }
